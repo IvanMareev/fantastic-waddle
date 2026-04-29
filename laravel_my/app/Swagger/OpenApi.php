42083b956d492d1e25a887afe54fa61cd8b2c4be
ivanmareev <enmareev17@gmail.com>
2026-04-29 21:50:39 +1000

    Fix gitignore

---
+++ laravel_my/app/Swagger/OpenApi.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Swagger;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *     title="AI Interview Trainer API",
+ *     version="1.0.0",
+ *     description="API documentation for AI Interview Trainer"
+ * )
+ *
+ * @OA\Server(
+ *     url="http://localhost/api",
+ *     description="Local API Server"
+ * )
+ */
+class OpenApi
+{
+}
